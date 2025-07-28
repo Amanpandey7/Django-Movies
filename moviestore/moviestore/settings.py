55998535a47b782a7b7c147aa402b1d22d8b3e4e
Amanpandey7 <Aman711pandey@gmail.com>
2025-07-29 00:21:57 +0530

    Listing of movies and individual

--- moviestore/moviestore/settings.py
+++ moviestore/moviestore/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home',
+    'movies',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS = [
     BASE_DIR / 'moviestore/static/',
 ]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
