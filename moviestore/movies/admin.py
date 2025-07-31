bd5470e617ec94e24c111dd2d4016a0a9e89864c
Amanpandey7 <Aman711pandey@gmail.com>
2025-07-31 23:09:46 +0530

    Review functionality has been added

--- moviestore/movies/admin.py
+++ moviestore/movies/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Review
 
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['id']
     search_fields = ['name', 'description']
 
 admin.site.register(Movie, MovieAdmin)
+admin.site.register(Review)
 # Register your models here.
